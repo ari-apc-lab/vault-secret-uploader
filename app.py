be75d95bb5c7fd732b9d3ea70a5cca0a979f0a3f
Jesús Ramos Rivas <jesus.ramos.external@atos.net>
2021-07-20 13:10:02 +0000

    fixes

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from base64 import b64encode
 from os import getenv
 from flask import Flask, request
 from requests import post, Session, adapters
+import json
 
 app = Flask(__name__)
 dashboard_urls = {}
@@ -11,7 +12,7 @@ user_ids = {}
 oidc_client_id = getenv("OIDC_CLIENT_ID", "sodalite-ide")
 oidc_client_secret = getenv("OIDC_CLIENT_SECRET", "")
 oidc_introspection_endpoint = getenv("OIDC_INTROSPECTION_ENDPOINT", "")
-vault_address = getenv("VAULT_ADDRESS", "")
+vault_endpoint = getenv("VAULT_ADDRESS", "") + ":" + getenv("VAULT_PORT", "8200")
 vault_admin_token = getenv("VAULT_ADMIN_TOKEN", "")
 
 session = Session()
@@ -35,34 +36,34 @@ def upload_hpc_secret():
     if "hpc" in json_data:
         hpc_name = json_data["hpc"]
     else:
-        return "Request must include HPC name (\"hpc\")", 403
+        return "Request must include HPC name (\"hpc\")\n", 403
 
-    if "user" in json_data:
+    if "ssh_user" in json_data:
         ssh_user = json_data["ssh_user"]
     else:
-        return "Request must include SSH user (\"ssh_user\")", 403
+        return "Request must include SSH user (\"ssh_user\")\n", 403
 
     ssh_pw = json_data["ssh_password"] if "ssh_password" in json_data else ""
     ssh_pkey = json_data["ssh_pkey"] if "ssh_pkey" in json_data else ""
 
     if not ssh_pw and not ssh_pkey:
-        return "Request must include either SSH password (\"ssh_pw\") or SSH private key (\"ssh_pkey\")", 403
+        return "Request must include either SSH password (\"ssh_pw\") or SSH private key (\"ssh_pkey\")\n", 403
 
     auth_header = {"x-vault-token": vault_admin_token}
     json_policy = {
-        "policy": "path \"hpc\\" + username + "\\*  {\n"
+        "policy": "path \"hpc/" + username + "/*\"\n{\n"
                   "  capabilities = [\"create\", \"read\", \"update\", \"delete\", \"list\"]\n"
                   "}"
     }
 
     json_role = {
-        "policies": "[" + username + "]",
+        "policies": ["hpc-" + username],
         "role_type": "jwt",
         "bound_audiences": "account",
         "user_claim": "email",
         "groups_claim": "",
         "bound_claims": {
-            "preferred_username": "hpc\\" + username
+            "preferred_username": username
         }
     }
 
@@ -73,26 +74,29 @@ def upload_hpc_secret():
     else:
         json_secret["private_key"] = ssh_pkey
 
-    policy_endpoint = "http://" + vault_address + "/v1/sys/policy/hpc/" + username
-    role_endpoint = "http://" + vault_address + "/v1/auth/jwt/role/" + username
-    secret_endpoint = "http://" + vault_address + "/v1/hpc/" + username + "/" + hpc_name
+    policy_endpoint = "http://" + vault_endpoint + "/v1/sys/policy/hpc-" + username
+    role_endpoint = "http://" + vault_endpoint + "/v1/auth/jwt/role/" + username
+    secret_endpoint = "http://" + vault_endpoint + "/v1/hpc/" + username + "/" + hpc_name
 
-    policy_response = post(policy_endpoint, data=json_policy, headers=auth_header)
+    policy_response = post(policy_endpoint, data=json.dumps(json_policy), headers=auth_header)
 
     if not policy_response.ok:
-        return "There was a problem creating the policy\n" + str(policy_response.content), 500
+        return ("There was a problem creating the policy:\n" + str(policy_response.content) + "\n",
+                policy_response.status_code)
 
-    binding_response = post(role_endpoint, data=json_role, headers=auth_header)
+    role_response = post(role_endpoint, data=json.dumps(json_role), headers=auth_header)
 
-    if not binding_response.ok:
-        return "There was a problem binding the keycloak username to the policy\n" + str(policy_response.content), 500
+    if not role_response.ok:
+        return ("There was a problem binding the keycloak user to the policy:\n" + str(role_response.content) + "\n",
+                role_response.status_code)
 
-    secret_response = post(secret_endpoint, data=json_secret, headers=auth_header)
+    secret_response = post(secret_endpoint, data=json.dumps(json_secret), headers=auth_header)
 
     if not secret_response.ok:
-        return "There was a problem creating the secret\n" + str(policy_response.content), 500
-    else:
-        return "Secret uploaded correctly", 200
+        return ("There was a problem creating the secret:\n" + str(secret_response.content) + "\n",
+                secret_response.status_code)
+
+    return "Secret uploaded correctly\n", 200
 
 
 def _token_info(access_token) -> dict:
@@ -100,7 +104,7 @@ def _token_info(access_token) -> dict:
     req = {'token': access_token}
     headers = {'Content-type': 'application/x-www-form-urlencoded'}
     if not oidc_introspection_endpoint:
-        raise Exception("No oidc_introspection_endpoint set on the server")
+        raise Exception("No oidc_introspection_endpoint set on the server\n")
 
     basic_auth_string = '{0}:{1}'.format(oidc_client_id, oidc_client_secret)
     basic_auth_bytes = bytearray(basic_auth_string, 'utf-8')
